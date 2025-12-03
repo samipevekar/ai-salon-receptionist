f429a5f20a93d9312ba1e7aa626e9ddc8e5b3326
samipevekar <samipevekar499@gmail.com>
2025-12-03 20:24:39 +0530

    test database

--- app/api/test-db/route.js
+++ app/api/test-db/route.js
@@ -1,6 +1,7 @@
 export async function GET() {
   return Response.json({
     env: process.env.DATABASE_URL ? "FOUND" : "NOT_FOUND",
+    env2: process.env.DATABASE_URL,
     OPENMIC_API_KEY: process.env.OPENMIC_API_KEY ? "FOUND" : "NOT_FOUND"
   });
 }
